08506c9cac329b1e6b7ac065c7850d68bd518c68
chaetesh <MY_NAME@example.com>
2024-09-22 16:49:14 +0530

    done

--- src/App.jsx
+++ src/App.jsx
@@ -31,7 +31,7 @@ function App() {
       }
       setError(null);
       const result = await axios.post(
-        "https://bajaj-backend-o3c0.onrender.com/bfhl",
+        "http://54.91.48.33:3000/bfhl",
         jsonData
       );
       setResponse(result.data);
